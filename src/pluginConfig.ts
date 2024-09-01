e9a8279894fbfb4f613a4f2c2445c4a99d76c03c
“wetterheidi“ <43071660+wetterheidi@users.noreply.github.com>
2024-09-01 13:26:11 +0200

    Added MFF option

--- src/pluginConfig.ts
+++ src/pluginConfig.ts
@@ -2,9 +2,9 @@ import type { ExternalPluginConfig } from '@windy/interfaces';
 
 const config: ExternalPluginConfig = {
     name: 'windy-plugin-upper-winds',
-    version: '0.0.6',
-    icon: '🪂',
-    title: 'Upper winds',
+    version: '0.0.1',
+    icon: '⚔',
+    title: 'MFF winds',
     description: 'Show upper winds, temperature and humidity at a given position',
     author: 'Heidi Schmid',
     repository: 'https://github.com/wetterheidi/windy-plugin-upper-winds.git',
