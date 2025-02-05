994fd30f58454fa506deb15b61b30c775323d37c
“wetterheidi“ <43071660+wetterheidi@users.noreply.github.com>
2025-02-05 17:27:54 +0100

    Official version 0.0.19

--- src/pluginConfig.ts
+++ src/pluginConfig.ts
@@ -2,7 +2,7 @@ import type { ExternalPluginConfig } from '@windy/interfaces';
 
 const config: ExternalPluginConfig = {
     name: 'windy-plugin-upper-winds',
-    version: '0.0.18',
+    version: '0.0.19',
     icon: '🪂',
     title: 'Upper winds',
     description: 'Show upper winds, temperature and humidity at a given position',
